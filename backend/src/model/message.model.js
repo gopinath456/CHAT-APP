3d2d24b0c186731d0eb73dd7e635424fdfa22a62
gopinath <topotop123456789@gamil.com>
2025-01-14 14:17:14 +0530

    created the Message model

---
+++ backend/src/model/message.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+// created message Schema
+
+const schema=new mongoose.Schema({
+    senderId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        require:true
+    },
+    receiverId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        require:true
+    },
+    text:{
+        type:String,
+    },
+    image:{
+        typ:String,
+    }
+},{timestamps:true})
+
+//created the Message model
+const Message=mongoose.model('Message',schema);
+
+export default Message;
